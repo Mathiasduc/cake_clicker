99b30e9ff984ea8e57cf5467f11c7cfa6b857403
Mathias Ducancel <mathiasduc@gmail.com>
2016-09-30 15:30:16 +0200

    Everything working
    fixed a line that i did delete by mistake

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ $(document).ready(function(){
 	var clickity = 0;
 	var kitty = "\\    /\\ <br> )  ( ')<br>(  /  ) <br> \\(__)|<br>";
 	$("img").click(function(){
+		clickity++;
 		$("span").html(clickity);
 		if(clickity % 20 === 0) {
-			var game = ["Tower Fall Ascencion", "Mini Metro", "This is police", "Truc","Machin", "Kingdoms", "This War of Mine"];
+			var game = ["Tower Fall Ascencion", "Mini Metro", "This is police", "Morrowind","The Stanlay Parable", "Kingdoms", "This War of Mine"];
 			alert("Allez jouer a un vrai jeu! Genre: "+ game[Math.floor(Math.random() * game.length)]);
-			console.log("Allez jouer a un vrai jeu! Genre: "+ game);
 		}
-
 	});
-
-	
-
 });
