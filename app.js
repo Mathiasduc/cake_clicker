c43f83f77c57cbbbad8cf07ea6069d9158503bb1
Mathias Ducancel <mathiasduc@gmail.com>
2016-09-30 15:21:37 +0200

    Add the click incramentation and alert pop-up
    but the code is broken , fixing that asap.

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-	//votre code
+	var clickity = 0;
+	var kitty = "\\    /\\ <br> )  ( ')<br>(  /  ) <br> \\(__)|<br>";
+	$("img").click(function(){
+		$("span").html(clickity);
+		if(clickity % 20 === 0) {
+			var game = ["Tower Fall Ascencion", "Mini Metro", "This is police", "Truc","Machin", "Kingdoms", "This War of Mine"];
+			alert("Allez jouer a un vrai jeu! Genre: "+ game[Math.floor(Math.random() * game.length)]);
+			console.log("Allez jouer a un vrai jeu! Genre: "+ game);
+		}
 
+	});
+
+	
 
 });
