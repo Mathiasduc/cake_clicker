e46c3ba411796f1957f8fdf8a7d2b232c32ca3d7
Mathias Ducancel <mathiasduc@gmail.com>
2016-10-07 14:41:27 +0200

    Cleaned the files

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 	var clickity = 0;
-	var kitty = "\\    /\\ <br> )  ( ')<br>(  /  ) <br> \\(__)|<br>";
 	$("img").click(function(){
 		clickity++;
 		$("span").html(clickity);
 		if(clickity % 20 === 0) {
 			var game = ["Tower Fall Ascencion", "Mini Metro", "This is police", "Morrowind","The Stanlay Parable", "Kingdoms", "This War of Mine"];
-			alert("Allez jouer a un vrai jeu! Genre: "+ game[Math.floor(Math.random() * game.length)]);
+			alert("Allez jouer a un vrai jeu! Genre: "+ game[Math.floor(Math.random() * game.length)]);	
 		}
 	});
 });
